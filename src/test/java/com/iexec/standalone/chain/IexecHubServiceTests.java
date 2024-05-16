ad26ba4ca9cf299ca1b04d9d3b2640bd6b9e8b1a
Nathan GAUD <nathan.gaud@gmail.com>
2024-05-16 10:31:55 +0200

    Add tests for test coverage

--- src/test/java/com/iexec/standalone/chain/IexecHubServiceTests.java
+++ src/test/java/com/iexec/standalone/chain/IexecHubServiceTests.java
@@ -16,12 +16,14 @@
 
 package com.iexec.standalone.chain;
 
+import com.iexec.common.contribution.Contribution;
 import com.iexec.commons.poco.chain.*;
 import com.iexec.commons.poco.contract.generated.IexecHubContract;
 import com.iexec.commons.poco.utils.BytesUtils;
 import com.iexec.standalone.config.BlockchainAdapterConfigurationService;
 import io.reactivex.Flowable;
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -37,15 +39,17 @@ import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.RemoteFunctionCall;
 import org.web3j.protocol.core.methods.response.Log;
 import org.web3j.protocol.core.methods.response.TransactionReceipt;
+import org.web3j.utils.Numeric;
 
 import java.time.Duration;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.ExecutionException;
 import java.util.function.BiFunction;
 import java.util.stream.Stream;
 
-import static com.iexec.commons.poco.utils.TestUtils.CHAIN_TASK_ID;
 import static com.iexec.commons.poco.utils.TestUtils.WORKER_ADDRESS;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -428,6 +432,226 @@ class IexecHubServiceTests {
         final ChainReceipt chainReceipt = eventBlockGetter.apply(chainConfigIexecHubService, fromBlock);
         assertThat(chainReceipt)
                 .isEqualTo(ChainReceipt.builder().build());
+    }
+    // endregion
+
+    private TransactionReceipt createReceiptWithoutLogs(List<Log> web3Logs) {
+        TransactionReceipt transactionReceipt = new TransactionReceipt();
+        transactionReceipt.setBlockNumber("0x1");
+        transactionReceipt.setGasUsed("0x186a0");
+        transactionReceipt.setLogs(web3Logs);
+        return transactionReceipt;
+    }
+
+    // region contribute
+    @Test
+    void shouldContribute() throws Exception {
+        String workerAddress = Numeric.toHexStringNoPrefixZeroPadded(
+                Numeric.toBigInt(credentials.getAddress()), 64);
+        Log web3Log = new Log();
+        web3Log.setData("0x1a538512b510ee384ce649b58a938d5c2df4ace50ef51d33f353276501e95662");
+        web3Log.setTopics(List.of(TASK_CONTRIBUTE_NOTICE, CHAIN_TASK_ID, workerAddress));
+        TransactionReceipt transactionReceipt = createReceiptWithoutLogs(List.of(web3Log));
+        when(iexecHubContract.contribute(any(), any(), any(), any(), any(), any())).thenReturn(remoteFunctionCall);
+        when(remoteFunctionCall.send()).thenReturn(transactionReceipt);
+        doReturn(true).when(BACSIexecHubService).isSuccessTx(any(), any(), any());
+
+        final Contribution contribution = Contribution.builder()
+                .chainTaskId(CHAIN_TASK_ID)
+                .enclaveChallenge("enclaveChallenge")
+                .enclaveSignature("enclaveSignature")
+                .resultHash("resultHash")
+                .resultSeal("resultSeal")
+                .workerPoolSignature("workerPoolSignature")
+                .build();
+        IexecHubContract.TaskContributeEventResponse response = BACSIexecHubService.contribute(contribution);
+        Assertions.assertThat(response).isNotNull();
+    }
+
+    @Test
+    void shouldNotContributeOnExecutionException() throws ExecutionException, InterruptedException {
+        final Contribution contribution = Contribution.builder()
+                .chainTaskId(CHAIN_TASK_ID)
+                .enclaveChallenge("enclaveChallenge")
+                .enclaveSignature("enclaveSignature")
+                .resultHash("resultHash")
+                .resultSeal("resultSeal")
+                .workerPoolSignature("workerPoolSignature")
+                .build();
+        doThrow(ExecutionException.class).when(BACSIexecHubService).submit(any());
+        IexecHubContract.TaskContributeEventResponse response = BACSIexecHubService.contribute(contribution);
+        Assertions.assertThat(response).isNull();
+    }
+
+    @Test
+    void shouldNotContributeWhenInterrupted() throws ExecutionException, InterruptedException {
+        final Contribution contribution = Contribution.builder()
+                .chainTaskId(CHAIN_TASK_ID)
+                .enclaveChallenge("enclaveChallenge")
+                .enclaveSignature("enclaveSignature")
+                .resultHash("resultHash")
+                .resultSeal("resultSeal")
+                .workerPoolSignature("workerPoolSignature")
+                .build();
+        doThrow(InterruptedException.class).when(BACSIexecHubService).submit(any());
+        IexecHubContract.TaskContributeEventResponse response = BACSIexecHubService.contribute(contribution);
+        Assertions.assertThat(response).isNull();
+        Assertions.assertThat(Thread.currentThread().isInterrupted()).isTrue();
+    }
+    // endregion
+
+    // region reveal
+    @Test
+    void shouldReveal() throws Exception {
+        String workerAddress = Numeric.toHexStringNoPrefixZeroPadded(
+                Numeric.toBigInt(credentials.getAddress()), 64);
+        Log web3Log = new Log();
+        web3Log.setData("0x88f79ce47dc9096bab83327fb3ae0cd99694fd36db6b5f22a4e4e7bf72e79989");
+        web3Log.setTopics(List.of(TASK_REVEAL_NOTICE, CHAIN_TASK_ID, workerAddress));
+        TransactionReceipt transactionReceipt = createReceiptWithoutLogs(List.of(web3Log));
+        when(iexecHubContract.reveal(any(), any())).thenReturn(remoteFunctionCall);
+        when(remoteFunctionCall.send()).thenReturn(transactionReceipt);
+        doReturn(true).when(BACSIexecHubService).isSuccessTx(any(), any(), any());
+
+        IexecHubContract.TaskRevealEventResponse response = BACSIexecHubService.reveal(CHAIN_TASK_ID, "resultDigest");
+        Assertions.assertThat(response).isNotNull();
+    }
+
+    @Test
+    void shouldNotRevealOnExecutionException() throws ExecutionException, InterruptedException {
+        doThrow(ExecutionException.class).when(BACSIexecHubService).submit(any());
+        IexecHubContract.TaskRevealEventResponse response = BACSIexecHubService.reveal(CHAIN_TASK_ID, "resultDigest");
+        Assertions.assertThat(response).isNull();
+    }
+
+    @Test
+    void shouldNotRevealWhenInterrupted() throws ExecutionException, InterruptedException {
+        doThrow(InterruptedException.class).when(BACSIexecHubService).submit(any());
+        IexecHubContract.TaskRevealEventResponse response = BACSIexecHubService.reveal(CHAIN_TASK_ID, "resultDigest");
+        Assertions.assertThat(response).isNull();
+        Assertions.assertThat(Thread.currentThread().isInterrupted()).isTrue();
+    }
+    // endregion
+
+    // region contributeAndFinalize
+    @Test
+    void shouldContributeAndFinalize() throws Exception {
+        Log web3Log = new Log();
+        web3Log.setData("0x000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000597b202273746f72616765223a202269706673222c20226c6f636174696f6e223a20222f697066732f516d6435763668723848385642444644746f777332786978466f76314833576f704a317645707758756d5a37325522207d00000000000000");
+        web3Log.setTopics(List.of(TASK_FINALIZE_NOTICE, CHAIN_TASK_ID));
+        TransactionReceipt transactionReceipt = createReceiptWithoutLogs(List.of(web3Log));
+        when(iexecHubContract.contributeAndFinalize(any(), any(), any(), any(), any(), any(), any())).thenReturn(remoteFunctionCall);
+        when(remoteFunctionCall.send()).thenReturn(transactionReceipt);
+        doReturn(true).when(BACSIexecHubService).isSuccessTx(any(), any(), any());
+
+        final Contribution contribution = Contribution.builder()
+                .chainTaskId(CHAIN_TASK_ID)
+                .enclaveChallenge("enclaveChallenge")
+                .enclaveSignature("enclaveSignature")
+                .resultDigest("resultDigest")
+                .workerPoolSignature("workerPoolSignature")
+                .build();
+        Optional<ChainReceipt> chainReceipt = BACSIexecHubService.contributeAndFinalize(contribution, "resultLink", "callbackData");
+        Assertions.assertThat(chainReceipt).isNotEmpty();
+    }
+
+    @Test
+    void shouldNotContributeAndFinalizeOnExecutionException() throws Exception {
+        final Contribution contribution = Contribution.builder()
+                .chainTaskId(CHAIN_TASK_ID)
+                .enclaveChallenge("enclaveChallenge")
+                .enclaveSignature("enclaveSignature")
+                .resultDigest("resultDigest")
+                .workerPoolSignature("workerPoolSignature")
+                .build();
+        doThrow(ExecutionException.class).when(BACSIexecHubService).submit(any());
+        Optional<ChainReceipt> chainReceipt = BACSIexecHubService.contributeAndFinalize(contribution, "resultLink", "callbackData");
+        Assertions.assertThat(chainReceipt).isEmpty();
+    }
+
+    @Test
+    void shouldNotContributeAndFinalizeWhenInterrupted() throws Exception {
+        final Contribution contribution = Contribution.builder()
+                .chainTaskId(CHAIN_TASK_ID)
+                .enclaveChallenge("enclaveChallenge")
+                .enclaveSignature("enclaveSignature")
+                .resultDigest("resultDigest")
+                .workerPoolSignature("workerPoolSignature")
+                .build();
+        doThrow(InterruptedException.class).when(BACSIexecHubService).submit(any());
+        Optional<ChainReceipt> chainReceipt = BACSIexecHubService.contributeAndFinalize(contribution, "resultLink", "callbackData");
+        Assertions.assertThat(chainReceipt).isEmpty();
+        Assertions.assertThat(Thread.currentThread().isInterrupted()).isTrue();
+    }
+    // endregion
+
+    // region isSuccessTx
+    @ParameterizedTest
+    @EnumSource(value = ChainContributionStatus.class)
+    void shouldTxBeSuccess(ChainContributionStatus chainContributionStatus) {
+        Log log = new Log();
+        log.setType("");
+        Assertions.assertThat(BACSIexecHubService.isSuccessTx(CHAIN_TASK_ID, log, chainContributionStatus)).isTrue();
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = ChainContributionStatus.class)
+    void shouldTxNotBeSuccessWhenLogIsNull(ChainContributionStatus chainContributionStatus) {
+        Assertions.assertThat(BACSIexecHubService.isSuccessTx(CHAIN_TASK_ID, null, chainContributionStatus)).isFalse();
+    }
+
+    @Test
+    void shouldTxNotBeSuccessWhenTimeout() {
+        Log log = new Log();
+        log.setType("pending");
+        when(web3jService.getBlockTime()).thenReturn(Duration.ofMillis(100L));
+        doReturn(Optional.empty()).when(BACSIexecHubService).getChainContribution(CHAIN_TASK_ID);
+        Assertions.assertThat(BACSIexecHubService.isSuccessTx(CHAIN_TASK_ID, log, ChainContributionStatus.CONTRIBUTED)).isFalse();
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = ChainContributionStatus.class)
+    void test(ChainContributionStatus chainContributionStatus) {
+        Log log = new Log();
+        log.setType("pending");
+        ChainContribution chainContribution = ChainContribution.builder().status(chainContributionStatus).build();
+        when(web3jService.getBlockTime()).thenReturn(Duration.ofMillis(100L));
+        doReturn(Optional.of(chainContribution)).when(BACSIexecHubService).getChainContribution(CHAIN_TASK_ID);
+        Assertions.assertThat(BACSIexecHubService.isSuccessTx(CHAIN_TASK_ID, log, chainContributionStatus)).isTrue();
+    }
+    // endregion
+
+    // region ChainTask status
+    @ParameterizedTest
+    @EnumSource(value = ChainTaskStatus.class, names = "ACTIVE")
+    void shouldChainTaskBeActive(ChainTaskStatus chainTaskStatus) {
+        doReturn(Optional.of(ChainTask.builder().status(chainTaskStatus).build()))
+                .when(BACSIexecHubService).getChainTask(CHAIN_TASK_ID);
+        Assertions.assertThat(BACSIexecHubService.isChainTaskActive(CHAIN_TASK_ID)).isTrue();
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = ChainTaskStatus.class, names = "ACTIVE", mode = EnumSource.Mode.EXCLUDE)
+    void shouldChainTaskNotBeActive(ChainTaskStatus chainTaskStatus) {
+        doReturn(Optional.of(ChainTask.builder().status(chainTaskStatus).build()))
+                .when(BACSIexecHubService).getChainTask(CHAIN_TASK_ID);
+        Assertions.assertThat(BACSIexecHubService.isChainTaskActive(CHAIN_TASK_ID)).isFalse();
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = ChainTaskStatus.class, names = "REVEALING")
+    void shouldChainTaskBeRevealing(ChainTaskStatus chainTaskStatus) {
+        doReturn(Optional.of(ChainTask.builder().status(chainTaskStatus).build()))
+                .when(BACSIexecHubService).getChainTask(CHAIN_TASK_ID);
+        Assertions.assertThat(BACSIexecHubService.isChainTaskRevealing(CHAIN_TASK_ID)).isTrue();
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = ChainTaskStatus.class, names = "REVEALING", mode = EnumSource.Mode.EXCLUDE)
+    void shouldChainTaskNotBeRevealing(ChainTaskStatus chainTaskStatus) {
+        doReturn(Optional.of(ChainTask.builder().status(chainTaskStatus).build()))
+                .when(BACSIexecHubService).getChainTask(CHAIN_TASK_ID);
+        Assertions.assertThat(BACSIexecHubService.isChainTaskRevealing(CHAIN_TASK_ID)).isFalse();
     }
     // endregion
 }
---
+++ src/test/java/com/iexec/standalone/feign/LoginServiceTests.java
@@ -0,0 +1,160 @@
+/*
+ * Copyright 2022-2023 IEXEC BLOCKCHAIN TECH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.iexec.standalone.feign;
+
+import com.iexec.commons.poco.security.Signature;
+import com.iexec.commons.poco.utils.SignatureUtils;
+import com.iexec.standalone.chain.CredentialsService;
+import com.iexec.standalone.feign.client.CoreClient;
+import feign.FeignException;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
+import org.web3j.crypto.Credentials;
+import org.web3j.crypto.ECKeyPair;
+import org.web3j.crypto.Keys;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import static com.iexec.standalone.feign.LoginService.TOKEN_PREFIX;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(OutputCaptureExtension.class)
+class LoginServiceTests {
+
+    @Mock
+    CoreClient coreClient;
+    @Mock
+    CredentialsService credentialsService;
+    @InjectMocks
+    private LoginService loginService;
+
+    @BeforeEach
+    void init() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @SneakyThrows
+    private Credentials generateCredentials() {
+        ECKeyPair ecKeyPair = Keys.createEcKeyPair();
+        return Credentials.create(ecKeyPair);
+    }
+
+    @Test
+    void shouldNotLoginOnBadChallengeStatusCode() {
+        Credentials credentials = generateCredentials();
+        when(credentialsService.getCredentials()).thenReturn(credentials);
+        when(coreClient.getChallenge(credentials.getAddress())).thenThrow(FeignException.class);
+        assertAll(
+                () -> assertEquals("", loginService.login()),
+                () -> verify(coreClient).getChallenge(credentials.getAddress())
+        );
+    }
+
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(strings = "")
+    void shouldNotLoginOnEmptyChallenge(String challenge) {
+        Credentials credentials = generateCredentials();
+        when(credentialsService.getCredentials()).thenReturn(credentials);
+        when(coreClient.getChallenge(credentials.getAddress())).thenReturn(challenge);
+        assertAll(
+                () -> assertEquals("", loginService.login()),
+                () -> verify(coreClient).getChallenge(credentials.getAddress())
+        );
+    }
+
+    @Test
+    void shouldNotLoginOnBadLoginStatusCode() {
+        Credentials credentials = generateCredentials();
+        when(credentialsService.getCredentials()).thenReturn(credentials);
+        when(coreClient.getChallenge(credentials.getAddress())).thenReturn("challenge");
+        Signature signature = SignatureUtils.hashAndSign("challenge", credentials.getAddress(), credentials.getEcKeyPair());
+        when(coreClient.login(credentials.getAddress(), signature)).thenThrow(FeignException.class);
+        assertAll(
+                () -> assertEquals("", loginService.login()),
+                () -> verify(coreClient).getChallenge(credentials.getAddress()),
+                () -> verify(coreClient).login(credentials.getAddress(), signature)
+        );
+    }
+
+    @NullSource
+    @ValueSource(strings = "")
+    @ParameterizedTest
+    void shouldNotLoginOnEmptyToken(String token) {
+        Credentials credentials = generateCredentials();
+        when(credentialsService.getCredentials()).thenReturn(credentials);
+        when(coreClient.getChallenge(credentials.getAddress())).thenReturn("challenge");
+        Signature signature = SignatureUtils.hashAndSign("challenge", credentials.getAddress(), credentials.getEcKeyPair());
+        when(coreClient.login(credentials.getAddress(), signature)).thenReturn(token);
+        assertAll(
+                () -> assertEquals("", loginService.login()),
+                () -> verify(coreClient).getChallenge(credentials.getAddress()),
+                () -> verify(coreClient).login(credentials.getAddress(), signature)
+        );
+    }
+
+    @Test
+    void shouldLogin() {
+        Credentials credentials = generateCredentials();
+        when(credentialsService.getCredentials()).thenReturn(credentials);
+        when(coreClient.getChallenge(credentials.getAddress())).thenReturn("challenge");
+        Signature signature = SignatureUtils.hashAndSign("challenge", credentials.getAddress(), credentials.getEcKeyPair());
+        when(coreClient.login(credentials.getAddress(), signature)).thenReturn("token");
+        assertAll(
+                () -> assertEquals(TOKEN_PREFIX + "token", loginService.login()),
+                () -> verify(coreClient).getChallenge(credentials.getAddress()),
+                () -> verify(coreClient).login(credentials.getAddress(), signature)
+        );
+    }
+
+    @Test
+    void shouldLoginOnceOnSimultaneousCalls(CapturedOutput output) throws InterruptedException, ExecutionException, TimeoutException {
+        Credentials credentials = generateCredentials();
+        when(credentialsService.getCredentials()).thenReturn(credentials);
+        when(coreClient.getChallenge(credentials.getAddress())).thenReturn("challenge");
+        Signature signature = SignatureUtils.hashAndSign("challenge", credentials.getAddress(), credentials.getEcKeyPair());
+        when(coreClient.login(credentials.getAddress(), signature)).thenReturn("token");
+        CompletableFuture<Void> run1 = CompletableFuture.runAsync(() -> loginService.login());
+        CompletableFuture<Void> run2 = CompletableFuture.runAsync(() -> loginService.login());
+        CompletableFuture<Void> run3 = CompletableFuture.runAsync(() -> loginService.login());
+        CompletableFuture.allOf(run1, run2, run3).get(1L, TimeUnit.SECONDS);
+        assertThat(output.getOut())
+                .contains("login already ongoing", "login already ongoing", "Retrieved new JWT token from scheduler");
+        assertAll(
+                () -> verify(coreClient).getChallenge(credentials.getAddress()),
+                () -> verify(coreClient).login(credentials.getAddress(), signature)
+        );
+    }
+
+}
